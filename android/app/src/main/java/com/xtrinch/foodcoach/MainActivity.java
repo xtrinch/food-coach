5cd2e2e376369a5d342941df0a98109b2b28a463
xtrinch <mojca.rojko@povio.com>
2025-11-26 20:06:27 +0100

    Capacitor init

---
+++ android/app/src/main/java/com/xtrinch/foodcoach/MainActivity.java
@@ -0,0 +1,5 @@
+package com.xtrinch.foodcoach;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.xtrinch.foodcoach',
+  appName: 'Food Coach',
+  webDir: 'dist'
+};
+
+export default config;
